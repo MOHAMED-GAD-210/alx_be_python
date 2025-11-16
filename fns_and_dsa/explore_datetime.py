e21f5594320fbf00234e6ae5c01042d41a0f961c
MOHAMED-GAD-210 <arabianights200001@gmail.com>
2025-11-16 15:59:38 -0800

    fix

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,10 +1,12 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+
+egypt_tz = timezone(timedelta(hours=2))
 
 def display_current_datetime():
     """
     Display the current date and time in YYYY-MM-DD HH:MM:SS format
     """
-    current_date = datetime.now()
+    current_date = datetime.now(egypt_tz)
     print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
     return current_date
 
@@ -12,7 +14,7 @@ def calculate_future_date(days_to_add):
     """
     Calculate the future date by adding the given number of days to current date
     """
-    current_date = datetime.now()
+    current_date = datetime.now(egypt_tz)
     future_date = current_date + timedelta(days=days_to_add)
     print("Future date:", future_date.strftime("%Y-%m-%d"))
     return future_date
